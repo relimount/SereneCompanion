4af7dd39fb6ae3dc4134d013ce0dcfb73918190c
胡唐峰 <152974699+relimount@users.noreply.github.com>
2026-01-09 09:57:15 +0800

    配置路由

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Layout from '@/views/Main.vue'
+import Login from '@/views/login/index.vue'
+
+const routes = [
+    {
+        path: '/',
+        component: Layout,
+    },
+    {
+        path: '/login',
+        component: Login,
+    },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+export default router
